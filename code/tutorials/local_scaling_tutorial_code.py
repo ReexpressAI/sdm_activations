53837d4125abbb5af987afdee8ce9951b827526c
Allen Schmaltz <allen.schmaltz@gmail.com>
2025-12-20 18:38:51 -0600

    Additional tutorial code

--- code/tutorials/local_scaling_tutorial_code.py
+++ code/tutorials/local_scaling_tutorial_code.py
@@ -14,6 +14,8 @@ def local_soft_sdm_max(batch_input, q, distance_quantile_per_class=None, log=Fal
         assert batch_input.shape == distance_quantile_per_class.shape
     q_rescale_offset = 2
     q_factor = q_rescale_offset + q
+    # The offset here assumes every column of d for a given batch (i.e., row) is the same value, as is standard with
+    # SDM activations:
     batch_input = batch_input - torch.amax(batch_input, dim=1, keepdim=True)  # for numerical stability
     if distance_quantile_per_class is not None:
         rescaled_distribution = q_factor ** (batch_input * distance_quantile_per_class)
@@ -42,3 +44,70 @@ def local_soft_sdm_max_log_to_probability(batch_input, q):
     q_rescale_offset = 2
     q_factor = q_rescale_offset + q
     return q_factor ** batch_input
+
+
+def local_soft_sdm_max_torch_equivalent(batch_input, q, distance_quantile_per_class=None, log=False,
+                                        change_of_base=True):
+    """
+        Alternatively, we can avoid reimplementing numerical stability code and instead use standard softmax
+        in existing frameworks by using the relation (2+q)^(z'*d) == e^(z'*d*log_e(2+q)), given that 2+q > 0.
+        In this case, it is still necessary to rescale the log in the
+        loss (or as for example, (cross-entropy loss per instance) / log_e(2+q)) to multiply the loss by
+        1/log_e(2+q) relative to the rescaled CE loss. See also local_soft_sdm_max_torch_equivalent_streamlined().
+    """
+    assert len(batch_input.shape) == 2
+    assert batch_input.shape[0] == q.shape[0]
+    assert q.shape[1] == 1
+    if distance_quantile_per_class is not None:
+        assert batch_input.shape == distance_quantile_per_class.shape
+    q_rescale_offset = 2
+    q_factor = q_rescale_offset + q
+    log_q_factor = torch.log(q_factor)
+    if distance_quantile_per_class is not None:
+        scaled_logits = batch_input * distance_quantile_per_class * log_q_factor
+    else:
+        scaled_logits = batch_input * log_q_factor
+    if log:
+        log_probs = torch.nn.functional.log_softmax(scaled_logits, dim=-1)  # log_softmax for numerical stability
+        if change_of_base:
+            return log_probs / log_q_factor
+        else:
+            return log_probs
+    else:
+        return torch.softmax(scaled_logits, dim=-1)
+
+
+# For reference:
+def local_soft_sdm_max_torch_equivalent_streamlined(batch_input, q, distance_quantile_per_class=None,
+                                                    return_scaled_logits_and_log_q_factor=False):
+    """
+        This uses the relation (2+q)^(z'*d) == e^(z'*d*log_e(2+q)), given that 2+q > 0, in order to use existing
+        optimized softmax and CE loss implementations.
+
+        If return_scaled_logits=True, this can be used with the standard CE loss after scaling by log_q_factor:
+            loss_fn = nn.CrossEntropyLoss(reduction='none')
+            scaled_logits, log_q_factor = local_soft_sdm_max_torch_equivalent_streamlined(
+                batch_z_prime, batch_q, distance_quantile_per_class=batch_d,
+                return_scaled_logits=True
+            )
+            # Note the .squeeze() is necessary for the correct broadcast:
+            loss_for_batch = loss_fn(scaled_logits, targets) / log_q_factor.squeeze()
+            loss_for_batch.mean().backward()  # Note the .mean()
+            # Because an absence of .squeeze() could be a source of subtle bugs, we return log_q_factor.squeeze() here.
+    """
+    assert len(batch_input.shape) == 2
+    assert batch_input.shape[0] == q.shape[0]
+    assert q.shape[1] == 1
+    if distance_quantile_per_class is not None:
+        assert batch_input.shape == distance_quantile_per_class.shape
+    q_rescale_offset = 2
+    q_factor = q_rescale_offset + q
+    log_q_factor = torch.log(q_factor)
+    if distance_quantile_per_class is not None:
+        scaled_logits = batch_input * distance_quantile_per_class * log_q_factor
+    else:
+        scaled_logits = batch_input * log_q_factor
+    if return_scaled_logits_and_log_q_factor:
+        return scaled_logits, log_q_factor.squeeze()
+    else:
+        return torch.softmax(scaled_logits, dim=-1)
